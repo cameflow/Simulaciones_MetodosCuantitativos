d5d426db9251a31790fe95ebe51e4ea2bb5768af
cameflow <alex.terrazas.z@gmail.com>
2016-10-18 11:44:14 -0500

    :rocket: File for queue simulation

---
+++ Colas/Colas.py
@@ -0,0 +1,16 @@
+l = 24
+m = 30
+
+L = l/(m-l)
+Lq = (l*l)/(m*(m-l))
+W = 1/(m-l)
+Wq = l/(m*(m-l))
+ro = l/m
+P0 = 1-ro
+
+print ("L:",L)
+print ("Lq:",Lq)
+print ("W:",round(W,3))
+print ("Wq:",round(Wq,3))
+print ("Ro:",round(ro,3))
+print ("P0:",round(P0,3))
