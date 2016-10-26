6bcce374a6f0c74cbd2f6ee879a7242e376b170a
cameflow <alex.terrazas.z@gmail.com>
2016-10-25 19:17:25 -0500

    :bug: Fix bugs

--- Generacion_Aleatorios/CongruenciaMixto.py
+++ Generacion_Aleatorios/CongruenciaMixto.py
@@ -8,10 +8,10 @@
 # a -> el multiplicador (a>0)
 # c -> constante aditiva (c>0)
 # m -> el modulo (m>X0, m>a y m>c)
-semilla = 117
+semilla = 27
 cantNumeros = 100
-a = 43
-c =  0
+a = 8
+c =  47
 m = 100
 
 listaNumeros = []
@@ -22,17 +22,18 @@ cola = 0
 ciclo = 0
 
 cantAleatorios = 0
+listaNumeros.append(semilla)
 #-------- Generación de números aleatorios ---------#
 while (cantAleatorios < cantNumeros):
     xn = (a * semilla + c) % m
 
     if ((len(listaNumeros)!=0) & repite == False):
-        cont = 0
+        cont = 1
         for i in listaNumeros:
             if(i == xn):
                 repite = True
                 valorRep = i
-                cola = cantAleatorios
+                cola = listaNumeros.index(i)
                 ciclo = cantAleatorios - cont
                 periodo = cola + ciclo
             cont += 1
@@ -44,8 +45,8 @@ while (cantAleatorios < cantNumeros):
 
 cont3 = 0
 for i in listaNumeros:
-    cont3 +=1
     print ("X"+ str(cont3)+": "+ str(i))
+    cont3 +=1
 print ("Valor repetido: " + str(valorRep))
 print ("Cola: "+str(cola))
 print ("Ciclo: "+str(ciclo))
